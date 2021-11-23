7cd40cac3f04ebd1ce8f1a59cbc6f087393fe3ff
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-11-23 16:21:24 +0000

    Removed <=, changed last elif operator comparison to just "else" as there are no other conditions, so this is the result by default.

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ bmi = round(weight / height ** 2)
 
 if bmi < 18.5:
   print(f"Your bmi is {bmi}, you are underweight")
-elif bmi <= 25:
+elif bmi < 25:
   print(f"Your bmi is {bmi}, your weight is normal")
 elif bmi < 30:
   print(f"Your bmi is {bmi}, you are overweight")
 elif bmi < 35:
   print(f"Your bmi is {bmi}, you are obese")
-elif bmi <= 35:
-  print(f"Your bmi is{bmi}, you are clinically obese")
+else:
+  print(f"Your bmi is {bmi}, you are clinically obese")
 
 
 
